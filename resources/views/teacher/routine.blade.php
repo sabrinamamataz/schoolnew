cc22dad7a08fbd806625afea0bc7165e71c03b79
Your name here <your_email@example.com>
2022-06-16 00:35:26 +0600

    table design for teacher routine

--- resources/views/teacher/routine.blade.php
+++ resources/views/teacher/routine.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="container">
         <div class="table-responsive">
-            <table class="table">
+            <table class="table table-striped table-hover">
                 <thead>
                     <tr>
                         <th scope="col">Day</th>
